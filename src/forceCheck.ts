19be13b0ad3d26d40ef8b23c188d4f52a9010561
Piotr Kałamarski <pkalamarski@gmail.com>
2020-11-23 11:25:35 +0100

    Delayed start fix

---
+++ src/forceCheck.ts
@@ -0,0 +1,16 @@
+import { google } from 'googleapis'
+
+import getPrices from './getPrices'
+import { logAction } from './logging'
+import initializeAuth from './authorize'
+
+const forceCheck = async () => {
+  const auth = initializeAuth()
+
+  const sheets = google.sheets({ version: 'v4', auth })
+
+  await logAction('Check triggered manually', sheets)
+  await getPrices(sheets)
+}
+
+export default forceCheck
