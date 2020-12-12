a73249574a062a50a73ec25da6f38a3a64a782f9
Piotr Kałamarski <pkalamarski@gmail.com>
2020-12-12 14:49:20 +0100

    ✨ Add basic product price display (#15)

--- client/components/PricePanel.tsx
+++ client/components/PricePanel.tsx
@@ -1,7 +1,49 @@
 import React from 'react'
+import useAxios from 'axios-hooks'
 
-const PricePanel = ({ product }: { product }) => {
-  return <p>{product.label}</p>
+import { IProduct } from '../../server/models/Products'
+import { IPriceData } from '../../server/models/PriceData'
+
+interface IProps {
+  product: IProduct
+}
+
+const PricePanel = ({ product }: IProps) => {
+  const [{ data: priceData, loading }] = useAxios<IPriceData>({
+    url: '/api/productPrices',
+    params: { productId: product.id }
+  })
+
+  return (
+    <div style={{ marginBottom: 10 }}>
+      <span style={{ display: 'flex' }}>
+        <h3 style={{ marginRight: 10 }}>{product.label}</h3>
+        <h5 style={{ fontStyle: 'italic' }}>
+          <a href={product.url}>
+            {new URL(product.url).host.split('www.').join('')}
+          </a>
+        </h5>
+      </span>
+      <span>Last updated: {priceData?.updatedDate?.toLocaleString()}</span>
+      <div style={{ display: 'flex' }}>
+        {priceData &&
+          priceData.prices
+            .sort(
+              (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+            )
+            .map((price, i) => (
+              <span style={{ flexFlow: 'column', marginRight: 15 }} key={i}>
+                <p>
+                  Price: {price.main} {priceData.currency}
+                </p>
+                {i === 0 && <p>Current price</p>}
+                {i > 0 && <p>Date: {price.date.toLocaleString()}</p>}
+              </span>
+            ))}
+      </div>
+      <hr />
+    </div>
+  )
 }
 
 export default PricePanel
