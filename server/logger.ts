50c9f7f03d1287243346341ead7df40df735bcc8
Piotr Kałamarski <pkalamarski@gmail.com>
2020-12-14 18:43:49 +0100

    Remove date from logs

--- server/logger.ts
+++ server/logger.ts
@@ -1,18 +1,15 @@
 import winston from 'winston'
 
-const formatMessage = (message: string): string =>
-  `${new Date().toISOString()} ${message}`
-
 export const logger = winston.createLogger({
   format: winston.format.simple(),
   transports: [new winston.transports.Console()]
 })
 
 export const logVerbose = (message: string): winston.Logger =>
-  logger.log('verbose', formatMessage(message))
+  logger.log('verbose', message)
 
 export const logInfo = (message: string): winston.Logger =>
-  logger.log('info', formatMessage(message))
+  logger.log('info', message)
 
 export const logError = (message: string): winston.Logger =>
-  logger.log('error', formatMessage(message))
+  logger.log('error', message)
