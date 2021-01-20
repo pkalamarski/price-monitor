03b2f0f2de52c01897520f817810d0448c06d80f
Piotr Kałamarski <pkalamarski@gmail.com>
2021-01-20 17:41:43 +0100

    lint

--- server/controllers/MappingController.ts
+++ server/controllers/MappingController.ts
@@ -1,4 +1,3 @@
-import { Inject } from '@decorators/di'
 import { Request, Response } from 'express'
 import { Controller, Get } from '@decorators/express'
 
@@ -6,8 +5,6 @@ import SiteMapping from '../models/SiteMapping'
 
 @Controller('/api/mapping')
 export default class BaseController {
-  constructor() {}
-
   @Get('/')
   async mapping(req: Request, res: Response): Promise<void> {
     const mapping = await SiteMapping.getAll()
